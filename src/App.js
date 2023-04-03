ee9e2657ddcb1ebb1a2c815e66a4f53918816a9d
musafaila <failatechnology@gmail.com>
2023-04-03 04:00:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Yo it's a brand new day!
         </a>
       </header>
     </div>
