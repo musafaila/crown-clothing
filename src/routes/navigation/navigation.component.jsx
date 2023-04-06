86f3acfe18a031d6263e7f1f9691d1afc7cf0a1a
musafaila <failatechnology@gmail.com>
2023-04-06 09:16:21 +0100

    Styled our navigation component and added our Logo.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,24 @@
 import { Fragment } from "react";
-
 import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to={"/"}>
-          <div className="logo">Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
 
         <div className="nav-links-container">
           <Link className="nav-link" to={"/shop"}>
             Shop
           </Link>
-        </div>
-        <h1>I am the Nav Bar</h1>
-        <Outlet />
+        </div>       
       </div>
+      <Outlet />
     </Fragment>
   );
 };
